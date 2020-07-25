c5d7dbbc8d951a5897b2eac54c43f8dcd48c34b2
frezeer <frezee13@hotmail.com>
2020-07-25 17:57:40 -0500

    Sistema de Articulos trabajando correctamente

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,11 +1,35 @@
 class ArticlesController < ApplicationController
+
+       def show
+        @article = Article.find(params[:id])
+       end
+
+       def edit
+        """
+            @article = Article.find(params['id'])
+        """    
+             @article = Article.find(params['id'])
+              puts "\n\n\n #{@article.persisted?} \n\n\n"
+            
+       end 
+
+       def update
+         @article = Article.find(params[:id])
+         @article.update(title: params[:article][:title],content: params[:article][:content])
+
+            redirect_to @article
+       end 
        def new
         @article = Article.new 
+       """
         @article.title = 'Demo'
-    end
+       """  
+       end
     
-    def create
-       @article = Article.create(title: params[:article][:title])
-       render json: @article
-    end   
+       def create
+         @article = Article.create(title: params[:article][:title],content: params[:article][:content])
+         render json: @article
+       end
+
+
 end
