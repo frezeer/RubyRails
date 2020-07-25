6ba7dff026593eb45e0e8dfa8cf7ea718a130921
frezeer <frezee13@hotmail.com>
2020-07-25 16:37:10 -0500

    se crea articles y funciona

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   get 'home/index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get  "/bienvenida" , to: "home#index"  
-  get  "article/new" , to: "article#new"
-  post "articles",     to: "article#create"
+  get  "articles/new" , to: "articles#new"
+  post "articles",     to: "articles#create"
 end
