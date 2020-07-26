b50b4214a095407dbd2c6585545ec24bf61e5339
frezeer <frezee13@hotmail.com>
2020-07-26 01:19:25 -0500

    Se crea scaffold de Category

--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,25 @@
 Rails.application.routes.draw do
 
+  resources :categories
   devise_for :users
- # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  root                      to: "home#index" 
-  get  "articles/user/:user_id", to: "articles#from_author" 
-  get  "articles",          to: "articles#index"
-  get  "/bienvenida" ,      to: "home#index" 
-  get  "articles/new" ,     to: "articles#new"   , as: :new_articles
-  get  "articles/:id" ,     to: "articles#show" 
-  get  "articles/:id/edit", to: "articles#edit"
 
-  patch "articles/:id",     to: "articles#update" ,  as: :article
-  post  "articles",         to: "articles#create"
-  delete "articles/:id",    to: "articles#delete"   
+
+  
+ # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root                      to: "home#index"
+
+  resources :articles do
+      get "user/:user_id" , to: "articles#from_author", on: :collection 
+  end
+  # get  "articles/user/:user_id", to: "articles#from_author" 
+  # get  "articles",          to: "articles#index"
+  # get  "/bienvenida" ,      to: "home#index" 
+  # get  "articles/new" ,     to: "articles#new"   , as: :new_articles
+  # get  "articles/:id" ,     to: "articles#show" 
+  # get  "articles/:id/edit", to: "articles#edit"
+  # patch "articles/:id",     to: "articles#update" ,  as: :article
+  # post  "articles",         to: "articles#create"
+  # delete "articles/:id",    to: "articles#delete"   
   
 end
 
